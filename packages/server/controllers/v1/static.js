245eda50e183eff14ee0ab77920b2d2ac75c61d9
青湛 <0x1304570@gmail.com>
2018-12-12 17:27:06 +0800

    feat(server): 新增 API 接口获取图形验证码 (#5)

--- packages/server/controllers/v1/static.js
+++ packages/server/controllers/v1/static.js
@@ -5,7 +5,7 @@ const readFile = util.promisify(fs.readFile);
 
 class Static {
   async getNorms(ctx) {
-    const data = await readFile('./assets/normss.md', 'utf-8');
+    const data = await readFile('./assets/norms.md', 'utf-8');
     ctx.body = data;
   }
 }
--- packages/server/test/app.tets.js
+++ packages/server/test/app.tets.js
@@ -5,9 +5,8 @@ const should = require('should');
 describe('test /app.js', function() {
   it('should / status 200', async function() {
     try {
-      const res = await request.get('/');
-
-      res.status.should.equal(404);
+      const res = await request.get('/').expect(404);
+      res.text.should.equal('请求的API地址不正确或者不存在');
     } catch(err) {
       should.ifError(err.message);
     }
