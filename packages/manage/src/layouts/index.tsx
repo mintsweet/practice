793f2bcc7bfebc73e4444a761ea5a756ba823d5a
mintsweet <0x1304570@gmail.com>
2019-02-28 10:42:50 +0800

    feat(manage): 初始化项目

---
+++ packages/manage/src/layouts/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './index.css';
+
+export type BasicLayoutComponent<P> = React.SFC<P>;
+
+export interface BasicLayoutProps extends React.Props<any> {
+  history?: History;
+  location?: Location;
+}
+
+const BasicLayout: BasicLayoutComponent<BasicLayoutProps> = props => {
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      { props.children }
+    </div>
+  );
+};
+
+export default BasicLayout;
