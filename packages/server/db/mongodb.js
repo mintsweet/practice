08eca49eb9f54a83699f86da7cc344281c3e8158
mintsweet <0x1304570@gmail.com>
2019-07-10 11:47:29 +0800

    chore(server): 重命名 test 目录为 __test__，区分测试数据库地址

--- packages/server/db/mongodb.js
+++ packages/server/db/mongodb.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 const { DB_PATH } = require('../../../config');
 
+const dbpath = process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/p-test' : DB_PATH;
+
 mongoose.set('useCreateIndex', true);
-mongoose.connect(DB_PATH, { useNewUrlParser: true });
+mongoose.connect(dbpath, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
--- packages/server/utils/db-server.sh
+++
@@ -1,5 +0,0 @@
-# start mongodb service
-mongod -f /usr/local/etc/mongod.conf
-
-# start redis service
-redis-server /usr/local/etc/redis.conf
