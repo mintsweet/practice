1ec3c27ab856e420b2b48d58ec361fad0b4721cd
mintsweet <0x1304570@gmail.com>
2021-07-19 16:00:15 +0800

    feat(manage): 更新 create-react-app 默认配置

--- packages/manage/src/App.tsx
+++ packages/manage/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button>button</Button>
+    </div>
+  );
 }
 
 export default App;
