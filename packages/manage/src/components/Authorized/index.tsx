ea8f5900e3cba1d5f6e3f7ed0822502e73450d59
mintsweet <0x1304570@gmail.com>
2021-08-10 10:22:18 +0800

    feat(manage): 扩展 Authorized 组件，支持无权额外显示元素

--- packages/manage/src/components/Authorized/index.tsx
+++ packages/manage/src/components/Authorized/index.tsx
@@ -3,10 +3,11 @@ import userStore from '@/store/user';
 
 interface Props {
   role?: number;
+  noAuth?: JSX.Element;
   children: JSX.Element;
 }
 
-export default function Authorized({ role, children }: Props) {
+export default function Authorized({ role, noAuth, children }: Props) {
   const loginStatus = userStore.useState('status');
   const userInfo = userStore.useState('info');
 
@@ -14,5 +15,5 @@ export default function Authorized({ role, children }: Props) {
     return <Redirect to="/login" />;
   }
 
-  return role && role > (userInfo?.role || 0) ? null : children;
+  return role && role > (userInfo?.role || 0) ? noAuth || null : children;
 }
