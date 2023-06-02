3db6cff8d5d16ad3ed22e13845bbc41c9a2fd21a
mintsweet <0x1304570@gmail.com>
2023-06-02 09:46:55 +0800

    feat(api): support passowrd encryption

--- packages/api/migrations/1685428173986-init-user-root.ts
+++ packages/api/migrations/1685428173986-init-user-root.ts
@@ -1,13 +1,18 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { User, UserRole } from '../src/entities/user.entity';
 
+const { SALT_ROUNDS, ROOT_PASS } = process.env;
+
 export class InitUserRoot1685428173986 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    const salt = bcrypt.genSaltSync(+SALT_ROUNDS);
+    const hash = bcrypt.hashSync(ROOT_PASS, salt);
     await queryRunner.manager.save(
       queryRunner.manager.create(User, {
         email: 'root@practice.com',
-        password: '123456',
+        password: hash,
         nickname: 'root',
         role: UserRole.ROOT,
       }),
