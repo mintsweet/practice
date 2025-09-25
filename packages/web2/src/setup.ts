ff00e2d1538be88ce70a7794b105cd9f461aa51e
mintsweet <0x1304570@gmail.com>
2025-09-26 11:46:33 +1200

    feat: add default toast to request

--- packages/web2/src/setup.ts
+++ packages/web2/src/setup.ts
@@ -1,7 +1,12 @@
 import { setupRequest, createCookieStrategy } from '@mints/request';
+import { toast } from '@mints/ui';
 
 setupRequest({
   baseURL: '/api',
+  toast: {
+    success: toast.success,
+    error: toast.error,
+  },
   auth: createCookieStrategy({
     refreshPath: '/api/auth/refresh',
     tokenField: 'accessToken',
