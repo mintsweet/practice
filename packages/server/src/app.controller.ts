115c3220413c55f83e87f8416cb3ea229152fc10
mintsweet <0x1304570@gmail.com>
2025-10-23 10:12:12 +1300

    feat: add default message to root

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,9 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
+  constructor(private readonly config: ConfigService) {}
+
+  @Get()
+  hello() {
+    return {
+      message: `${this.config.get('APP_NAME')} API is running!`,
+    };
+  }
+
   @Get('/health')
   health() {
-    return { status: 'OK' };
+    return {
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
